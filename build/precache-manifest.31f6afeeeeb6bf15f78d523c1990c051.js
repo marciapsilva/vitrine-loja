da7ca8106935b1b4ac7b2645366724fff49d5657
msilva <marcia.silva1802@gmail.com>
2018-12-17 02:58:24 -0200

    first commit

---
+++ build/precache-manifest.31f6afeeeeb6bf15f78d523c1990c051.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4306d9de6ca8adf25d2f",
+    "url": "/static/js/main.4306d9de.chunk.js"
+  },
+  {
+    "revision": "70e3bd33e4e598ac6590",
+    "url": "/static/js/1.70e3bd33.chunk.js"
+  },
+  {
+    "revision": "4306d9de6ca8adf25d2f",
+    "url": "/static/css/main.c5fee826.chunk.css"
+  },
+  {
+    "revision": "d228f02531a7db483b12eb55012e7e4a",
+    "url": "/index.html"
+  }
+];
